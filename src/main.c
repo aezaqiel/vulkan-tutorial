d510e6806feb3f60ee1f9a21561bf19a1cd22a7f
Aliff Ezaqiel Zaffrulla <aezaqiel@gmail.com>
2024-08-28 20:40:34 +0800

    initial project setup

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include <volk.h>
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+#include <cglm/cglm.h>
+
+int main(void)
+{
+    printf("hello world\n");
+    return 0;
+}
